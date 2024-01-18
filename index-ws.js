5ab2e65ae0886a3185821a35009bff576f0fd9ab
svedish <svevo@svevoromano.it>
2024-01-18 12:51:40 +0100

    Sviluppato il client

--- index-ws.js
+++ index-ws.js
@@ -13,5 +13,40 @@ server.on('request', app);
 // E mettiamo il server in ascolto sulla porta 3000 facendogli produrre una conferma a schermo
 server.listen(3000, function() { console.log('Server started on port 3000'); });
 
-/** Begin websocket */
 
+/** Inizio WebSocket */
+
+// Includiamo la libreria ws e ad essa chiediamo un server
+const WebSocketServer = require('ws').Server;
+
+// Creiamo una nuova istanza di WebSocketServer e indichiamo come server il server Node creato prima
+const wss = new WebSocketServer({server: server});
+
+// Vogliamo fare qualcosa quando il WebSocket si connette quindi creiamo una funzione connection a cui passiamo 
+wss.on('connection', function connection(ws) {
+    
+    // Raccogliamo il numero di client connessi e lo mostriamo in console
+	const numClients = wss.clients.size;
+    console.log('Clients connected', numClients);
+	
+    // Usando la funzione broadcast annunciamo a tutti i clients quanti visitatori attivi ci sono
+    wss.broadcast(`Current visitors: ${numClients}`);
+
+    // Inviamo al client un messaggio di benvenuto, verificando prima che lo stato della sua singola connessione sia OPEN
+    if (ws.readyState === ws.OPEN) { 
+        ws.send('Welcome to my server');
+    }
+    // Gestiamo infine il caso in cui una singola connesisone venga chiusa
+    ws.on('close', function close() {
+        wss.broadcast(`Current visitors: ${numClients}`);
+        console.log('A client has disconnected')
+    });
+
+});
+
+// Questa è la funzione di broadcast che invia dati a tutti i client connessi, usa un forEach in modo da inviare ciò che deve inviare a tutti in una volta
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    });
+}
